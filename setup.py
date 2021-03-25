1f2b9afb7597452f658892ce2082572990fe2adb
Martin Morset <mamor@dfds.com>
2021-03-25 12:51:01 +0100

    black formatting

--- setup.py
+++ setup.py
@@ -4,45 +4,49 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Martin Morset",
-    author_email='mamor@dfds.com',
-    python_requires='>=3.5',
+    author_email="mamor@dfds.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Thin high level client to use when consuming data through the Denodo proprietory ODBC driver.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='denodoclient',
-    name='denodoclient',
-    packages=find_packages(include=['denodoclient', 'denodoclient.*']),
+    keywords="denodoclient",
+    name="denodoclient",
+    packages=find_packages(include=["denodoclient", "denodoclient.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/dingobar/denodoclient',
-    version='0.1.0',
+    url="https://github.com/dingobar/denodoclient",
+    version="0.1.0",
     zip_safe=False,
 )
