4ffd7bcbc02598f976699f84b0a7ae303313c33b
Martin Morset <mamor@dfds.com>
2021-03-29 11:22:54 +0200

    Bump version: 0.2.0 → 0.3.0

--- denodoclient/__init__.py
+++ denodoclient/__init__.py
@@ -9,4 +9,4 @@ DenodoClient = DenodoClient
 
 __author__ = """Martin Morset"""
 __email__ = "mamor@dfds.com"
-__version__ = "__version__ = '0.2.0'"
+__version__ = "__version__ = '__version__ = "0.3.0"'"
